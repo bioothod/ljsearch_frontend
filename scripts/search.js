381770a4d14a7a6034d483e5091b421b6a45bf4c
Evgeniy Polyakov <zbr@ioremap.net>
2016-08-05 01:22:22 +0300

    Updated info and css

--- scripts/search.js
+++ scripts/search.js
@@ -1,15 +1,7 @@
 var Info = React.createClass({
-  onLinkClick: function(e) {
-    e.preventDefault();
-    this.props.onLinkClick(e.target.text);
-  },
   onQueryClick: function(e) {
     e.preventDefault();
     this.props.onQueryClick(e.target.text);
-  },
-  onAuthorClick: function(e) {
-    e.preventDefault();
-    this.props.onAuthorClick(e.target.text);
   },
 
   render: function() {
@@ -22,7 +14,7 @@ var Info = React.createClass({
         <ul>
           <li>Search is performed over stemmed content, spell checking is not yet supported, but indexed content includes both stemmed and original versions of the posts and comments.</li>
           <li>Search only supports AND operator, i.e. documents returned are guaranteed to contain all search query elements. Exact match is not yet supported.</li>
-          <li>Link searching form can include both url format like [<a href="" onClick={this.onLinkClick}>www.gazeta.ru</a>] or just [<a href="" onClick={this.onLinkClick}>jpg</a>], returned documents match both text query and have required link elements in 'a' or 'img' tags.</li>
+          <li>Link searching can include both url format like [<a href="" onClick={this.onQueryClick}>link:www.gazeta.ru</a>] or just [<a href="" onClick={this.onQueryClick}>link:jpg</a>], returned documents match both text query and have required link elements in 'a' or 'img' tags.</li>
           <li>Author search like [<a href="" onClick={this.onQueryClick}>author:drugoi.livejournal.com</a>] limits results to given author only, but please note that original database does not always have author field for content. If author is not specified, search is being performed against the whole database.</li>
           <li>There are no exceptions for stop words like [<a href="" onClick={this.onQueryClick}>to be or not to be</a>] or short terms like [<a href="" onClick={this.onQueryClick}>i</a>], everything is indexed and being quickly searched.</li>
           <li>Very small disk footprint, this test content is about 200Mb uncompressed (200k livejournal posts and comments), it takes about 450Mb on disk, including original content and general and per-author indexes.</li>
@@ -43,7 +35,7 @@ var Info = React.createClass({
 window.SearchForm = React.createClass({
   getInitialState: function() {
     return {
-      query: 'Путин',
+      query: '',
       post: true,
       comment: true,
       info_active: true,
@@ -82,12 +74,14 @@ window.SearchForm = React.createClass({
     return (
       <div className="searchWrapper">
         <form className="searchForm" onSubmit={this.handleSubmit}>
-          <div className="query">Search query: <input type="text" placeholder="Путин" value={this.state.query} onChange={this.handleQueryChange}/></div>
+          <div className="queryLine">
+          <input id="submit" type="submit" value="Post" />
+          <div className="query"><input type="text" placeholder="Livejournal archive search..." value={this.state.query} onChange={this.handleQueryChange}/></div>
+          </div>
           <div className="checkbox"><input type="checkbox" defaultChecked={true} onChange={this.handlePostCheckboxChange} />Search in posts</div>
           <div className="checkbox"><input type="checkbox" defaultChecked={true} onChange={this.handleCommentCheckboxChange} />Search in comments</div>
-          <input type="submit" value="Post" />
         </form>
-        <Info active={this.state.info_active} onLinkClick={this.onLinkClick} onAuthorClick={this.onAuthorClick} onQueryClick={this.onQueryClick}/>
+        <Info active={this.state.info_active} onQueryClick={this.onQueryClick}/>
       </div>
     );
   }
