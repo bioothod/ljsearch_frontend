0aa132b951f5f69d07e20c87730d0e82f4931c81
Evgeniy Polyakov <zbr@ioremap.net>
2016-08-05 00:24:00 +0300

    search: updated info text, get rid of unused states

--- scripts/search.js
+++ scripts/search.js
@@ -23,16 +23,16 @@ var Info = React.createClass({
           <li>Search is performed over stemmed content, spell checking is not yet supported, but indexed content includes both stemmed and original versions of the posts and comments.</li>
           <li>Search only supports AND operator, i.e. documents returned are guaranteed to contain all search query elements. Exact match is not yet supported.</li>
           <li>Link searching form can include both url format like [<a href="" onClick={this.onLinkClick}>www.gazeta.ru</a>] or just [<a href="" onClick={this.onLinkClick}>jpg</a>], returned documents match both text query and have required link elements in 'a' or 'img' tags.</li>
-          <li>Author search like [<a href="" onClick={this.onAuthorClick}>drugoi.livejournal.com</a>] limits results to given author only, but please note that original database does not always have author field for content. If author is not specified, search is being performed against the whole database.</li>
+          <li>Author search like [<a href="" onClick={this.onQueryClick}>author:drugoi.livejournal.com</a>] limits results to given author only, but please note that original database does not always have author field for content. If author is not specified, search is being performed against the whole database.</li>
           <li>There are no exceptions for stop words like [<a href="" onClick={this.onQueryClick}>to be or not to be</a>] or short terms like [<a href="" onClick={this.onQueryClick}>i</a>], everything is indexed and being quickly searched.</li>
           <li>Very small disk footprint, this test content is about 200Mb uncompressed (200k livejournal posts and comments), it takes about 450Mb on disk, including original content and general and per-author indexes.</li>
           <li>Per request type pagination support, there are 3 request types: post content and title and comment content. Pagination allows to read next set of search results for particular type without reloading others.</li>
+          <li>Exact phrase search - use double quotes [<a href="" onClick={this.onQueryClick}>"поездка Путина"</a>] to find out exact match, please note that stemming is not used in exact match, i.e. ["testing"] is not equal to ["test"] which would yield the same results if used without quotes.</li>
+          <li>Negation support - use 'minus' symbol [<a href="" onClick={this.onQueryClick}>-rafale</a>] to exclude documents which contain given word.</li>
         </ul>
         <p>TODO list:</p>
         <ul>
-          <li>Exact phrase search</li>
           <li>Spell checking and error correction</li>
-          <li>Negation support</li>
           <li>Date/Time search support</li>
         </ul>
       </div>
@@ -44,23 +44,14 @@ window.SearchForm = React.createClass({
   getInitialState: function() {
     return {
       query: 'Путин',
-      author: '',
-      links: '',
       post: true,
       comment: true,
       info_active: true,
-      next_id: 0,
     }
   },
 
   handleQueryChange: function(e) {
     this.setState({query: e.target.value});
-  },
-  handleAuthorChange: function(e) {
-    this.setState({author: e.target.value});
-  },
-  handleLinksChange: function(e) {
-    this.setState({links: e.target.value});
   },
   handlePostCheckboxChange: function(e) {
     this.setState({post: e.target.checked});
@@ -69,33 +60,22 @@ window.SearchForm = React.createClass({
     this.setState({comment: e.target.checked});
   },
 
-  onLinkClick: function(text) {
-    this.setState({links: text});
-  },
   onQueryClick: function(text) {
     this.setState({query: text});
   },
-  onAuthorClick: function(text) {
-    this.setState({author: text});
-  },
 
   handleSubmit: function(e) {
-    this.setState({info_active: false});
-
     e.preventDefault();
     var query = this.state.query.trim();
-    var author = this.state.author.trim();
-    var links = this.state.links.trim();
-    if (!query && !links) {
+    if (!query) {
       return;
     }
 
     this.props.onSubmit({
       query: query,
-      author: author,
-      links: links,
       post: this.state.post,
       comment: this.state.comment,
+      info_active: false,
     });
   },
   render: function() {
@@ -103,8 +83,6 @@ window.SearchForm = React.createClass({
       <div className="searchWrapper">
         <form className="searchForm" onSubmit={this.handleSubmit}>
           <div className="query">Search query: <input type="text" placeholder="Путин" value={this.state.query} onChange={this.handleQueryChange}/></div>
-          <div className="author">Search for embedded links containing given elements: <input type="text" placeholder="www.gazeta.ru" value={this.state.links} onChange={this.handleLinksChange}/></div>
-          <div className="author">Search only for this author: <input type="text" placeholder="xxx.livejournal.com" value={this.state.author} onChange={this.handleAuthorChange}/></div>
           <div className="checkbox"><input type="checkbox" defaultChecked={true} onChange={this.handlePostCheckboxChange} />Search in posts</div>
           <div className="checkbox"><input type="checkbox" defaultChecked={true} onChange={this.handleCommentCheckboxChange} />Search in comments</div>
           <input type="submit" value="Post" />
